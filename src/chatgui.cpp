1b23fbe9767aa2c25b1e47ff4f34fdc4932a2e55
Derek Zhi <zhiderek@gmail.com>
2020-04-30 11:47:02 -0700

    make _chatLogic a unique pointer

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -6,9 +6,11 @@
 #include "chatlogic.h"
 #include "chatgui.h"
 
+
 // size of chatbot window
 const int width = 414;
-const int height = 736;
+// const int height = 736;
+const int height = 600; //made to be compatible with my macbook monitor
 
 // wxWidgets APP
 IMPLEMENT_APP(ChatBotApp);
@@ -118,7 +120,8 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = new ChatLogic(); 
+    _chatLogic = std::make_unique<ChatLogic>();
+    // _chatLogic = new ChatLogic(); 
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
@@ -135,7 +138,7 @@ ChatBotPanelDialog::~ChatBotPanelDialog()
     //// STUDENT CODE
     ////
 
-    delete _chatLogic;
+    // delete _chatLogic;
 
     ////
     //// EOF STUDENT CODE
